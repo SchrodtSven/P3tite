6bc106d4cbffc196a57231cd1aabb586711a6732
Sven Schrodt <sven@schrodt.club>
2022-08-09 16:36:23 +0200

    minor changes

--- test/Core/FooTest.php
+++ test/Core/FooTest.php
@@ -12,9 +12,22 @@ use PHPUnit\Framework\TestCase;
 
 class FooTest extends TestCase
 {
-    public function testDouble()
+    /**
+     * @dataProvider doubleProvider
+     */
+    public function testDouble(int  $input, int $expected)
     {
         $bar = new \P3tite\Core\Foo();
-        $this->assertTrue($bar->doubleMe(45) === 90);
+        $this->assertTrue($bar->doubleMe($input) === $expected);
+    }
+
+    public function doubleProvider(): array
+    {
+        return [
+            [0, 0],
+            [1, 2],
+            [99, 198],
+            [5, 10]
+        ];
     }
 }
